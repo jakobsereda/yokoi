d33f0f1ed0da0d9e873131a040fbe1ef904999bc
jakob <138621739+jakobsereda@users.noreply.github.com>
2024-10-06 23:21:06 -0700

    feat: added args parsing and simple cpu tick

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,40 @@
-use crate::registers::Registers;
+use crate::registers::{self, Registers};
 
 pub struct CPU {
     registers: Registers,
 }
 
 impl CPU {
-    // TODO
+    pub fn new() -> Self {
+        let registers = Registers::new();
+        Self {
+            registers: registers,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let op = self.fetch();
+        self.execute(op);
+    }
+
+    fn fetch(&mut self) -> u8 {
+        0
+    }
+
+    fn execute(&mut self, op: u8) {
+        match op {
+            // nop
+            0x00 => { }
+
+            // ld r16, imm16
+            0x01 => {
+
+            }
+            
+            _ => panic!("Unimplemented opcode: {}", op)
+        }
+
+    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::{Result, Context};
+use std::{
+    fs::{self},
+    path::Path,
+};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to file
+    #[arg(short, long)]
+    path: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let expanded = shellexpand::tilde(&args.path).to_string();
+    let path = Path::new(&expanded);
+    let contents = fs::read_to_string(path)
+        .with_context(|| format!("Failed to read from file: {:#?}", path))?;
+    println!("{}", contents);
+    Ok(())
 }
